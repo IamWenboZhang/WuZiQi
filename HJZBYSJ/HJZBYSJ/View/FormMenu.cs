d1d411567220cac441cfb7133191e39e05d2d1c0
IamWenboZhang <zwbhehe@live.com>
2015-12-21 15:25:13 +0800

    Revert "Revert "上传五子棋源码""
    This reverts commit 2fe57a5823855eeaeb400799d3d1ddef5a29fd84.

---
+++ HJZBYSJ/HJZBYSJ/View/FormMenu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+//using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HJZBYSJ.View
+{
+    public partial class FormMenu : Form
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSingle_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void btnOnline_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Ignore;
+            this.Close();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+    }
+}
